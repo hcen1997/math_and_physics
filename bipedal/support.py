18524c7256071f6e146f3785c22bab6375b2a186
hcen1997 <860178804@qq.com>
2023-01-08 17:17:02 +0800

    画出以腿基点为中心的上半身

---
+++ bipedal/support.py
@@ -0,0 +1,272 @@
+# 通过展示单腿的支撑态, 来确定重心的移动过程
+import math
+
+import matplotlib.pyplot as plt
+from matplotlib import collections
+
+import numpy as np
+from numpy import pi, sin, cos, fmod
+
+
+class State:
+    def __init__(self):
+        self.height = 1.740
+        self.velocity = 1.2
+        self.single_step_time = 0.5
+        self.x_p_step = self.velocity * self.single_step_time
+        print("单步持续时间", self.single_step_time)
+        print("单步运动距离", self.x_p_step)
+
+        # 周期控制
+        self.index_in_loop = 0
+
+        self.head_r = 0.261 / 2
+        self.head_x = 0.500
+        self.body_h = 0.556
+        self.body_w = 0.237
+        self.leg_base_xy = np.array([self.head_x, self.height - 2 * self.head_r - self.body_h])
+        self.leg_center_angle = pi
+        self.leg_front_limit = pi / 6
+        self.leg_back_limit = -pi / 12
+        # 大腿长
+        self.leg1_length = 384
+        self.leg1_knee_xy = np.array([0, 0])
+        self.leg11_theta = 0
+        self.leg2_knee_xy = np.array([0, 0])
+        self.leg21_theta = 0
+
+        # 小腿长
+        self.leg2_length = 0.428
+        self.leg1_ankle_xy = np.array([0, 0])
+        self.leg12_theta = 0
+        self.leg2_ankle_xy = np.array([0, 0])
+        self.leg22_theta = 0
+        # 脚踝
+        self.leg_ankle_R = 0.048
+        # 脚掌
+        self.leg3_length = 0.237
+        self.leg3_back_length = 0.041
+        self.leg3_mid_length = 0.140
+        self.leg3_front_length = 0.06
+        self.leg13_theta = 0
+        self.leg13_front_xy = np.array([0, 0])
+        self.leg13_back_xy = np.array([0, 0])
+
+
+state = State()
+
+
+def plot_ref_line():
+    # 基础坐标轴  高为2米的线
+    ref_h = 2
+    # 参考线 x   长1米
+    ref_x = 1
+    # 参考线位于-300处
+    ref_offset = -0.1
+    plt.vlines(ref_offset, 0, ref_h, 'blue', ':', "垂直两米参考线")
+    # plt.hlines(ref_offset, 0, ref_x, 'black', ':', "水平1米参考线")
+    plt.hlines(0, ref_offset - 0.001, ref_x * 1.2, 'black', '--', "地面")
+    plt.hlines(0, ref_offset - 0.002, ref_x * 1.2, 'black', '--', )
+    plt.hlines(0, ref_offset - 0.003, ref_x * 1.2, 'black', '--', )
+    plt.hlines(0, ref_offset - 0.004, ref_x * 1.2, 'black', '--', )
+
+    plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+    plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+
+
+def plot_body():
+    # 画出腿部基点圆
+    leg_base = plt.Circle(state.leg_base_xy, 0.03, linestyle='-', fill=True, color='red', )
+    plt.gcf().gca().add_patch(leg_base)
+    body_xy = state.leg_base_xy - [state.body_w / 2, 0]
+    body = plt.Rectangle(body_xy, state.body_w, state.body_h, linestyle='-',
+                         fill=False)
+    plt.gcf().gca().add_patch(body)
+    head_xy = state.leg_base_xy + [0, state.body_h + state.head_r]
+    head = plt.Circle(head_xy, state.head_r, linestyle='-', fill=False)
+    plt.gcf().gca().add_patch(head)
+
+
+# python 支持 state.leg1_knee_xy  的写法, 但是不建议 因为为了统一信息来源
+
+def plot_leg11(dtheta=0):
+    # 向上为正
+    theta = state.leg_center_angle + state.leg_front_limit + dtheta
+    leg_xy_00 = [sin(theta) * state.leg1_length, cos(theta) * state.leg1_length]
+    leg_xy_00 = np.array(leg_xy_00)
+    lb = np.array(state.leg_base_xy)
+    plot_line([state.leg_base_xy, leg_xy_00 + lb], 'orange')
+    state.leg1_knee_xy = leg_xy_00 + lb
+    state.leg11_theta = theta
+
+
+def plot_line(line, color):
+    leg11 = collections.LineCollection([line], colors=color)
+    plt.gcf().gca().add_collection(leg11)
+
+
+def plot_leg21(dtheta=0):
+    theta = state.leg_center_angle + state.leg_back_limit + dtheta
+    leg_xy_00 = [sin(theta) * state.leg1_length, cos(theta) * state.leg1_length]
+    leg_xy_00 = np.array(leg_xy_00)
+    lb = np.array(state.leg_base_xy)
+    plot_line([state.leg_base_xy, leg_xy_00 + lb], 'cyan')
+    state.leg2_knee_xy = leg_xy_00 + lb
+    state.leg21_theta = theta
+
+
+def gen_ctl_dtheta_sig_in_period(ctl_step_duration, ctl_step_dt, ctl_dtheta_sig, ctl_step_period):
+    # 在整个周期中 每一步的序号, 分两条腿
+    length = int(ctl_step_duration / ctl_step_dt)  # bug 浮点数转int丢失精度
+    ctl_dtheta_sig_arr = [[], []]
+    t_index = 0
+    t_sum = 0
+    for i in range(length):
+        # 判断在哪个周期内
+        now = i * ctl_step_dt
+        if not now < t_sum + ctl_step_period[t_index]:
+            t_sum = t_sum + ctl_step_period[t_index]
+            t_index = t_index + 1
+        ctl_dtheta_sig_arr[0].append(ctl_dtheta_sig[0][t_index])
+        ctl_dtheta_sig_arr[1].append(ctl_dtheta_sig[1][t_index])
+    return ctl_dtheta_sig_arr
+
+
+"""
+画线子过程
+1. 基点
+2. plot_line([base, leg_xy_00 + base], <color_you_should_like>)
+"""
+
+
+def plot_leg12(dtheta):
+    # plot_line([state.leg1_knee_xy, (200, 200)], 'orange')
+
+    theta = state.leg11_theta + dtheta
+    color = 'orange'
+    # 大腿处于+-15度边界的时候, 腿平行, 其他时刻腿垂直
+    if abs(state.leg11_theta - state.leg_center_angle) < pi / 12 and state.index_in_loop < 10:
+        color = 'red'
+        # print(state.index_in_loop)
+    else:
+        # theta = state.leg_center_angle + dtheta
+        color = 'orange'
+
+    leg_xy_00 = np.array([sin(theta) * state.leg2_length, cos(theta) * state.leg2_length])
+
+    state.leg1_ankle_xy = leg_xy_00 + np.array(state.leg1_knee_xy)
+    plot_line([state.leg1_knee_xy, state.leg1_ankle_xy], color)  # 咦, 我只需要关心某些点就行了
+    t = (state.leg1_ankle_xy - state.leg1_knee_xy) ** 2
+    # print("leg2长度", math.sqrt(t[0] + t[1])) # leg2 长度没问题, 只是看起来有变化
+    state.leg12_theta = theta
+
+
+def plot_leg22(dtheta):
+    theta = state.leg_center_angle  # + state.leg_back_limit + dtheta
+    leg_xy_00 = np.array([sin(theta) * state.leg2_length, cos(theta) * state.leg2_length])
+    state.leg2_ankle_xy = leg_xy_00 + np.array(state.leg2_knee_xy)
+    plot_line([state.leg2_knee_xy, state.leg2_ankle_xy], 'cyan')  # 咦, 我只需要关心某些点就行了
+    state.leg22_theta = theta
+
+
+# log_
+def plot_knee():
+    knee1 = plt.Circle(state.leg1_knee_xy, 40, linestyle='-', fill=False, color='green')
+    plt.gcf().gca().add_patch(knee1)
+    # knee2 = plt.Circle(state.leg2_knee_xy, 40, linestyle='-', fill=False, color='green')
+    # plt.gcf().gca().add_patch(knee2)
+
+
+def plot_leg13(dtheta):
+    leg3_back_length = 41
+    print('脚踝距地面距离', state.leg1_ankle_xy[1])
+    # 脚掌2百3 最大距离240
+    # 这里简单写个离地面 20 高的运动情况
+    foot_from_ground = state.leg1_ankle_xy[1]
+    target_h = foot_from_ground - 80
+    target_dtheta = np.arccos(target_h / (state.leg3_length - leg3_back_length))
+
+    theta = state.leg_center_angle + target_dtheta  # + state.leg_back_limit + dtheta
+    leg_xy_o = np.array(state.leg1_ankle_xy)
+    leg_xy_front = np.array(
+        [sin(theta) * (state.leg3_length - leg3_back_length), cos(theta) * (state.leg3_length - leg3_back_length)])
+    # state.leg2_ankle_xy = leg_xy_00 + np.array(state.leg2_knee_xy)
+    theta = theta - pi
+    leg_xy_back = np.array([sin(theta) * (leg3_back_length), cos(theta) * (leg3_back_length)])
+    plot_line([leg_xy_front + leg_xy_o, leg_xy_back + leg_xy_o], 'orange')  # 咦, 我只需要关心某些点就行了
+    state.leg23_theta = theta + pi
+    print("指尖距地面高度", (leg_xy_front + leg_xy_o)[1])
+
+
+def job_print_robot_support():
+    plt.figure(figsize=(7, 6), )
+    plt.axes([0.12, 0.11, 0.90 / 2, 0.80])
+    plt.ion()
+    perid = 50  # 秒
+    dt = 0.05
+    dtheta_leg11, dtheta_leg12 = 0.0, 0.0
+    w = (state.leg_front_limit - state.leg_back_limit) / state.single_step_time
+    print("\n开始脚脚运动模拟")
+    print("大腿角速度rad/s", w)
+    for t in np.arange(0, perid, dt):
+        plt.cla()
+        plot_ref_line()
+        plot_body()
+
+        ############################# controller start ###########
+        ctl_step_duration = 2 * state.single_step_time
+        ctl_step_dt = dt
+        ctl_dtheta_sig = [[-1, 1],
+                          [1, -1]]
+        ctl_step_period = [state.single_step_time, state.single_step_time]
+        # 这里直接使用指定结果
+        # ctl_dtheta_sig_in_period = gen_ctl_dtheta_sig_in_period(ctl_step_duration, ctl_step_dt,
+        #                                                         ctl_dtheta_sig, ctl_step_period)
+        # print(ctl_dtheta_sig_in_period)
+        ctl_leg1_sig = [
+            [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, ]]
+        # 着地序列 3,4,5,6,7,8  前摆序列 10 11 12 13 14 15
+        da = -pi / 24
+        ctl_leg2_sig = [
+            [da * 3, da * 2, da * 0.5, 0, 0, 0, 0, 0, 0, da * 1,
+             da * 2, da * 3, da * 4, da * 5, da * 6, da * 7, da * 8, da * 7, da * 6, da * 4.5, ],
+            [-pi / 2, -pi / 2, -pi / 2, -pi / 2, -pi / 2, -pi / 2, -pi / 2, -pi / 2, -pi / 2, -pi / 2, -pi / 2, -pi / 2,
+             -pi / 2, -pi / 2, -pi / 2, -pi / 2, -pi / 2, -pi / 2, -pi / 2, -pi / 2, ]
+        ]
+        ######################### controller stop #############
+        # 在一个单步周期内, 走过整个区间的速度
+        # 根据当前时间, 计算dtheta
+        current_index_in_control_arr = int(fmod(t, ctl_step_duration) / dt)
+        state.index_in_loop = current_index_in_control_arr
+        dtheta_leg11 = dtheta_leg11 + w * dt * ctl_leg1_sig[0][current_index_in_control_arr]
+        # plot_leg11(dtheta_leg11)
+        dtheta_leg12 = dtheta_leg12 + w * dt * ctl_leg1_sig[1][current_index_in_control_arr]
+        # plot_leg21(dtheta_leg12)
+        plt.text(-240, 1900, f'当前时间={t: 2.1f}秒')
+        # print(f'当前时间 {t:.1f} 左膝盖xy {state.leg2_knee_xy} '
+        #       f'左大腿角 {state.leg11_theta} 右膝盖xy {state.leg1_knee_xy} 右大腿角 {state.leg21_theta}')
+
+        # 画出膝盖和小腿
+        # plot_knee()
+        dtheta_leg21 = ctl_leg2_sig[0][current_index_in_control_arr]
+        # plot_leg12(dtheta_leg21)
+        dtheta_leg22 = ctl_leg2_sig[1][current_index_in_control_arr]
+        # plot_leg22(dtheta_leg22)
+        # print(f'当前时间 {t:.1f} 左脚踝 {state.leg2_ankle_xy} 右脚踝 {state.leg1_ankle_xy}')
+        # f'左大腿角 {state.leg11_theta} 右膝盖xy {state.leg1_knee_xy} 右大腿角 {state.leg21_theta}')
+        """
+        从模拟中可以看出, 只有2连杆的情况下, 足端到地面有一个 delta_height 
+        那么脚面的功能就的出来了, 补足这个 delta_height
+        同时因为 脚面的补足对于地面只是一个点/球
+        所以需要大拇指来产生一个随时平行于地面的平面, 来产生更大的摩擦力
+        """
+
+        # plot_leg13(0)
+        plt.pause(dt)
+        # plt.pause(dt /dt)
+    plt.show()
+
+
+if __name__ == '__main__':
+    job_print_robot_support()
